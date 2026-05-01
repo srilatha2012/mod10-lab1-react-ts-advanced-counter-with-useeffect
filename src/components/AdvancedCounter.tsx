28adecfbe51afe2221cf1f7bbeaac242707cc722
srilatha2012 <srilathp2012@gmail.com>
2026-05-01 00:06:36 -0400

    Added Reset mechanism

--- src/components/AdvancedCounter.tsx
+++ src/components/AdvancedCounter.tsx
@@ -5,50 +5,54 @@ export function AdvancedCounter() {
     const [count, setCount] = useState<number>(0);
     const [history, setHistory] = useState<number[]>([]);
 
-    function handleIncrement() {
-        const newCount = count + 1;
+    //helper function to update the count
+    function updateCount(newCount: number) {
         setCount(newCount);
         setHistory((prevHistory) => [...prevHistory, newCount]);
 
     }
+
+    function handleIncrement() {
+        updateCount(count + 1);
+
+    }
     function handleDecrement() {
-         const newCount = count - 1;
-        setCount(newCount);
-        setHistory((prevHistory) => [...prevHistory, newCount]);
+        updateCount(count - 1);
     }
     function handleReset() {
         setCount(0);
-        setHistory((prevHistory) => [...prevHistory, 0]);
+        // setHistory((prevHistory) => [...prevHistory, 0]);
+        setHistory([]);
     }
-    
+
     //Auto-save count
-    useEffect(()=> {
-        const timerId = setTimeout(()=>{
-          localStorage.setItem("count",JSON.stringify(count));
-        },500)
-        
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            localStorage.setItem("count", JSON.stringify(count));
+        }, 500)
+
         return () => {
             clearTimeout(timerId);
         }
 
-    },[count]);
+    }, [count]);
 
     //keyboard event listener
-    useEffect(()=>{
-       function handleKeyDown(event: KeyboardEvent) {
-         if(event.key === "ArrowUp") {
-            setCount((prevCount) => prevCount+1);
-         }
-         if(event.key === "ArrowDown") {
-            setCount((prevCount) => prevCount-1);
-         }
-       }
-       document.addEventListener("keydown",handleKeyDown);
-       return () =>{
-        document.removeEventListener("keydown", handleKeyDown)
-       }
+    useEffect(() => {
+        function handleKeyDown(event: KeyboardEvent) {
+            if (event.key === "ArrowUp") {
+                updateCount(count + 1);
+            }
+            if (event.key === "ArrowDown") {
+                updateCount(count - 1);
+            }
+        }
+        document.addEventListener("keydown", handleKeyDown);
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown)
+        }
 
-    },[]);
+    }, [count]);
 
     return (
         <div>
